b1b2aa8b7d35b3782539171fb51e8c6bcd713cf7
Bartalis Krisztian <bartalis.krisztian@yahoo.com>
2020-11-28 19:22:36 +0200

    create business form

--- app/src/main/java/com/e/kalaka/fragments/CreateBusinessFragment.kt
+++ app/src/main/java/com/e/kalaka/fragments/CreateBusinessFragment.kt
@@ -1,21 +1,177 @@
 package com.e.kalaka.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
 import com.e.kalaka.R
+import com.e.kalaka.databinding.FragmentCreateBusinessBinding
+import com.e.kalaka.utils.Tag
+import com.google.android.material.chip.Chip
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 
 class CreateBusinessFragment : Fragment() {
 
+    private lateinit var binding: FragmentCreateBusinessBinding
+    private var tags = mutableListOf<String>()
+    private val emails= mutableListOf<String>()
+
+    companion object {
+        //image pick code
+        private val IMAGE_PICK_CODE = 1000;
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        getUserEmails()
+        Log.d("-----", "$emails")
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_business, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_create_business,
+            container,
+            false
+        )
+
+        //setupAutoCompleteView()
+        binding.chooseLogoButton.setOnClickListener{
+            pickImageFromGallery()
+        }
+
+        binding.createBusinessButton.setOnClickListener{
+
+        }
+
+        setUpSpinner()
+
+        binding.createBusinessTagContainer.removeAllViews()
+
+        return binding.root
+    }
+
+    private fun pickImageFromGallery() {
+        //Intent to pick image
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, CreateBusinessFragment.IMAGE_PICK_CODE)
+    }
+
+    private fun setupAutoCompleteView() {
+        Log.d("-----", "$emails")
+        val autoComplete = binding.userAutoComplete
+        val adapter = ArrayAdapter(binding.root.context, R.layout.spinner_item_layout, emails)
+        autoComplete.setAdapter(adapter)
+        autoComplete.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val email = emails[position]
+                addUserChip(email)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+    }
+
+    private fun addUserChip(email: Any) {
+
+    }
+
+    private fun getUserEmails() {
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("users")
+        myRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (data in dataSnapshot.children) {
+                    val email = data.child("email").value.toString()
+                    emails.add(email)
+                    Log.d("-----", "$emails")
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w("--------", "Failed to read value.", error.toException())
+            }
+        })
+    }
+
+    private fun setUpSpinner() {
+        val spinner = binding.tagSpinner
+        val arrayAdapter = ArrayAdapter(
+            binding.root.context,
+            R.layout.spinner_item_layout,
+            Tag.getTagList()
+        )
+        spinner.adapter = arrayAdapter
+
+        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val item  = Tag.getTags()[position]
+                if(!tags.contains(item.second)) {
+                    tags.add(item.second)
+                    addTagChip(item)
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+
+
+    }
+
+    private fun addTagChip(item: Pair<String, String>) {
+        val chipGroup = binding.createBusinessTagContainer
+        val chip = Chip(binding.root.context)
+        val paddingDp: Int = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            10F,
+            resources.displayMetrics
+        ).toInt()
+        chip.setPadding(paddingDp, paddingDp, paddingDp, paddingDp)
+        chip.text = item.second
+        val iconId = binding.root.context.resources.getIdentifier(
+            item.first,
+            "drawable",
+            binding.root.context.packageName
+        )
+        chip.chipIcon = ContextCompat.getDrawable(binding.root.context, iconId)
+        chip.isCloseIconVisible = true
+        chip.setCloseIconResource(R.drawable.ic_action_close)
+        chip.setOnCloseIconClickListener {
+            it as TextView
+            tags.remove(it.text)
+            chipGroup.removeView(chip)
+        }
+        chipGroup.addView(chip)
     }
 
 }
--- app/src/main/java/com/e/kalaka/utils/Tag.kt
+++ app/src/main/java/com/e/kalaka/utils/Tag.kt
@@ -4,11 +4,18 @@ package com.e.kalaka.utils
 class Tag {
     companion object {
         private val tags: List<Pair<String, String>> =
-            listOf(Pair("ic_necklace", "Jewelry"),
-                    Pair("ic_bag", "Bags"),
-                    Pair("ic_cup",  "Cups"),
-                    Pair("ic_shirt", "T-shirts"))
+            listOf(Pair("ic_necklace", "Ékszerek"),
+                    Pair("ic_bag", "Szatyrok"),
+                    Pair("ic_cup",  "Bögrék"),
+                    Pair("ic_shirt", "Pólók"))
         fun getTags(): List<Pair<String, String>> = tags
         fun tagCount(): Int = tags.size
+        fun getTagList(): ArrayList<String> {
+            val array = arrayListOf<String>()
+            tags.forEach{
+                array.add(it.second)
+            }
+            return array
+        }
     }
 }
