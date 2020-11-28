32c314249ad1af3603dd22cc26dc0452ab8060bc
Horvath Aron <haronka99@gmail.com>
2020-11-28 13:03:26 +0200

    navigation between regist and login done

--- app/src/main/java/com/e/kalaka/fragments/LoginFragment.kt
+++ app/src/main/java/com/e/kalaka/fragments/LoginFragment.kt
@@ -5,17 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.e.kalaka.R
+import com.e.kalaka.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
+private lateinit var binding : FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        binding.gotoRegisterButton.setOnClickListener {
+            Navigation.findNavController(binding.root)
+                .navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+
+
+        return binding.root
     }
 
 }
