cc282bacc481b8480be2cd7a0ae1c9415302cfe2
Császár Ákos <57495891+RockieHUN@users.noreply.github.com>
2020-11-28 12:09:08 +0200

    Data classes created

---
+++ app/src/main/java/com/e/kalaka/models/BusinessOrder.kt
@@ -0,0 +1,19 @@
+package com.e.kalaka.models
+
+data class BusinessOrder (
+    var address : String,
+    var city : String,
+    var comment : String,
+    var number : Int,
+    var orderId : Int,
+    var postcode : String,
+    var productId : Int,
+    var productName: String,
+    var status : String,
+    var time : String,
+    var total : Double,
+    var worker : String
+        )
+
+
+
