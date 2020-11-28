9b242f345471504bba0922b0f548531933f3b093
Császár Ákos <57495891+RockieHUN@users.noreply.github.com>
2020-11-28 13:15:26 +0200

    checkout

---
+++ app/src/main/java/com/e/kalaka/fragments/BusinessProfile.kt
@@ -0,0 +1,40 @@
+package com.e.kalaka.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.e.kalaka.R
+import com.e.kalaka.databinding.FragmentBusinessProfileBinding
+
+
+class BusinessProfile : Fragment() {
+
+    private lateinit var binding : FragmentBusinessProfileBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+       binding = DataBindingUtil.inflate(inflater,R.layout.fragment_business_profile,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+    }
+
+
+
+
+}
--- app/src/main/java/com/e/kalaka/utils/RealtimeDatabase.kt
+++ app/src/main/java/com/e/kalaka/utils/RealtimeDatabase.kt
@@ -2,15 +2,14 @@ package com.e.kalaka.utils
 
 import com.e.kalaka.models.User
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import java.math.BigInteger
 import java.security.MessageDigest
 
-class Database{
+class RealtimeDatabase{
 
 
-
-    companion object{
     }
 
 
-}
